3a92b895ddcc7128fc181fc4d7189d2c61e0e814
dmpalampa <dmpalampa@gmail.com>
2021-12-10 12:13:45 +0300

    'service'

---
+++ EmployeeServ/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess;
+using Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Repo;
+
+namespace Service.EmployeeServ
+{
+    public class EmployeeRepository : IEmpoyeeRepository
+    {
+        private IRepository<Employee> employeeRepository;
+
+        public EmployeeRepository(IRepository<Employee> employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        public Employee AddEmployee(Employee emp)
+        {
+            employeeRepository.Insert(emp);
+            return emp;
+        }
+
+        public Employee DeleteEmployee(int id)
+        {
+            Employee emp = employeeRepository.Get(id);
+            employeeRepository.Remove(emp);
+            employeeRepository.SaveChanges();
+            return emp;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employeeRepository.GetAll();
+        }
+
+        public Employee updateEmployee(Employee emp)
+        {
+            employeeRepository.Update(emp);
+            return emp;
+        }
+    }
+}
---
+++ EmployeeServ/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service.EmployeeServ
+{
+    public interface IEmpoyeeRepository
+    {
+        Employee AddEmployee(Employee emp);
+
+        IEnumerable<Employee> GetAllEmployees();
+
+        Employee DeleteEmployee(int id);
+
+        Employee updateEmployee(Employee emp);
+
+
+    }
+}
